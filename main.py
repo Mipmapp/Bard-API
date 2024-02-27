15b0b2465e19bfc26219582f8c9a1545d71573a1
Mipmapp <157580621+Mipmapp@users.noreply.github.com>
2024-02-27 12:49:06 +0000

    Bardapi

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
 import requests
 from bardapi import Bard
-from gemini import GeminiClient
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
@@ -13,27 +11,7 @@ async def read_root():
     index_html_content = file.read()
   return HTMLResponse(content=index_html_content)
 
-
-# Simple Message Response
-@app.post("/bard", response_class=HTMLResponse)
-async def gemini(input: str, cookie: str, cookieTS: str):
-  client = GeminiClient(cookie, "_ga=" + cookieTS, proxy=None)
-  await client.init(timeout=120)
-
-  try:
-    response = await client.generate_content(input)
-    json_response = {
-        "content":
-        response.text,
-        "images":
-        [image.url for image in response.images] if response.images else None
-    }
-    return json.dumps(json_response, indent=2)
-
-  except Exception as e:
-    return f"Error: {e}"
-
-
+# Gemini Response
 @app.post("/gemini")
 async def bardapi(question: str, cookie: str):
   bard = Bard(token=cookie)
