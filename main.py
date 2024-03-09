f07c4638892950d98e86dd48c54ac12068b06d09
Mipmapp <157580621+Mipmapp@users.noreply.github.com>
2024-03-09 01:05:03 +0000

    Updated Gemini

--- main.py
+++ main.py
@@ -2,46 +2,54 @@ import requests
 from bardapi import Bard
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from gemini import Gemini
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
-  with open('index.html', 'r') as file:
-    index_html_content = file.read()
-  return HTMLResponse(content=index_html_content)
+    with open('index.html', 'r') as file:
+        index_html_content = file.read()
+    return HTMLResponse(content=index_html_content)
 
-# Gemini Response
-@app.post("/gemini")
-async def bardapi(question: str, cookie: str):
-  bard = Bard(token=cookie)
-  res = bard.get_answer(question)
-  response = {"content": res["content"]}
-
-  if res["images"]:
-    response["images"] = res["images"]
-  return response
-
+# Render API
 @app.post("/render")
 async def render(commitId: str, renderId: str, serviceId: str):
-  url = "https://api.render.com/v1/services/" + serviceId + "/deploys"
-  payload = {
-    "clearCache": "do_not_clear",
-    "commitId": commitId
-  }
-  headers = {
-    "accept": "application/json",
-    "content-type": "application/json",
-    "authorization": "Bearer " + renderId
-  }
-  response = requests.post(url, json=payload, headers=headers)
-  return response.text
+    url = "https://api.render.com/v1/services/" + serviceId + "/deploys"
+    payload = {
+        "clearCache": "do_not_clear",
+        "commitId": commitId
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": "Bearer " + renderId
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    return response.text
 
+# Gemini API
+@app.post("/gemini")
+async def gemini(question: str, cookie: str):
+    cookies = {
+        "__Secure-1PSID": cookie
+    }
 
+    GeminiClient = Gemini(cookies=cookies)
+    res = GeminiClient.generate_content(question)
+    response = {"content": res.response_dict['candidates']['text']}
+    image_urls = []
+
+    if 'web_images' in res.response_dict['candidates']:
+        for image in res.response_dict['candidates']['web_images']:
+            image_urls.append(str(image.url))
+
+    if image_urls:
+        response["images"] = image_urls
+
+    return response
+
+# ___________ #
 if __name__ == '__main__':
-  import uvicorn
-  uvicorn.run(app, host="0.0.0.0", port=8080)
-
-#Secure_1PSID = "g.a000ggjsJRHOEx2kqKHTle7dzdiPpkQoBWmWZ-BTLmAKYwi3XRgV7P3Or388TM6TBnYa1-Jv_AACgYKAbQSAQASFQHGX2MiW7BHA3R36o-uNvZKNdVv7hoVAUF8yKpplEiBu2i26myoN5W3pAtU0076"
-#Secure_1PSIDTS = "_ga=" + "GA1.1.812864621.1707432529"
-#INPUT = "tell me something nice"
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
