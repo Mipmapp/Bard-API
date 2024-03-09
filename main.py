64d9ae9cee088c04d7eaa64bad55bdfe753aa79f
Mipmapp <157580621+Mipmapp@users.noreply.github.com>
2024-03-09 06:34:41 +0000

    Yolo

--- main.py
+++ main.py
@@ -8,48 +8,47 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    with open('index.html', 'r') as file:
-        index_html_content = file.read()
-    return HTMLResponse(content=index_html_content)
+  with open('index.html', 'r') as file:
+    index_html_content = file.read()
+  return HTMLResponse(content=index_html_content)
 
 # Render API
 @app.post("/render")
 async def render(commitId: str, renderId: str, serviceId: str):
-    url = "https://api.render.com/v1/services/" + serviceId + "/deploys"
-    payload = {
-        "clearCache": "do_not_clear",
-        "commitId": commitId
-    }
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "authorization": "Bearer " + renderId
-    }
-    response = requests.post(url, json=payload, headers=headers)
-    return response.text
+  url = "https://api.render.com/v1/services/" + serviceId + "/deploys"
+  payload = {
+    "clearCache": "do_not_clear",
+    "commitId": commitId
+  }
+  headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "authorization": "Bearer " + renderId
+  }
+  response = requests.post(url, json=payload, headers=headers)
+  return response.text
 
 # Gemini API
 @app.post("/gemini")
 async def gemini(question: str, cookie: str):
-    cookies = {
-        "__Secure-1PSID": cookie
-    }
+  cookies = {
+    "__Secure-1PSID" : cookie
+  }
+  
+  GeminiClient = Gemini(cookies=cookies)
+  res = GeminiClient.generate_content(question)
+  response = {"content": res.text}
+  image_urls = []
+  if res.web_images:
+    for image in res.web_images:
+      image_urls.append(image.url)
+  
+  if image_urls:
+    response["images"] = image_urls
 
-    GeminiClient = Gemini(cookies=cookies)
-    res = GeminiClient.generate_content(question)
-    response = {"content": res.response_dict['candidates']['text']}
-    image_urls = []
-
-    if 'web_images' in res.response_dict['candidates']:
-        for image in res.response_dict['candidates']['web_images']:
-            image_urls.append(str(image.url))
-
-    if image_urls:
-        response["images"] = image_urls
-
-    return response
+  return response
 
 # ___________ #
 if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+  import uvicorn
+  uvicorn.run(app, host="0.0.0.0", port=8080)
